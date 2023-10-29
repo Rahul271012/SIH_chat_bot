6f9ab4b0283a57ae2eff3bae7be791780bc54bc7
Rahul@271012 <rp3874651@gmail.com>
2023-10-29 14:03:38 +0530

    ADD Enter button

--- bot.js
+++ bot.js
@@ -1,35 +1,61 @@
-const container=document.querySelector(".container");
-const man=document.querySelector(".close");
-const chatinput=document.querySelector(".container_text_input input");
-const chatbutton=document.querySelector(".container_text_input i");
-const chatbox=document.querySelector(".container_text_content");
-const cross=document.querySelector("#cross");
+const container = document.querySelector(".container");
+const man = document.querySelector(".close");
+const chatinput = document.querySelector(".container_text_input input");
+const chatbutton = document.querySelector(".container_text_input i");
+const chatbox = document.querySelector(".container_text_content");
+const cross = document.querySelector("#cross");
 
 let message;
-const API_KEY = "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";
+const API_KEY = "Use your API Key";
 
-const createchatlist = (message,className)=>{
-        const chatli=document.createElement("div");
-        chatli.classList.add("chat" ,className);
-        let chatcontent = className === "incoming" ?  `<p></p>`: ` <span class="fa-solid fa-robot"></span><p></p>`;
-        chatli.innerHTML=chatcontent;
-        chatli.querySelector("p").textContent=message;
-        return chatli;
+function keydown(e) {
+    // e.preventDefault();
+    if(e.key == 'Enter'){
+        message = chatinput.value.trim();
+
+    // console.log(message);
+    if (!message) return;
+    chatinput.value = "";
+
+    chatbox.appendChild(createchatlist(message, "incoming"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+
+    setTimeout(() => {
+        const outgoingchat = createchatlist("Thinking.....", "outgoing")
+        chatbox.appendChild(outgoingchat);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+        generateresponse(outgoingchat);
+
+    }, 100);
+
+
+    }
+
+    // console.log(e.key);
+
+}
+const createchatlist = (message, className) => {
+    const chatli = document.createElement("div");
+    chatli.classList.add("chat", className);
+    let chatcontent = className === "incoming" ? `<p></p>` : ` <span class="fa-solid fa-robot"></span><p></p>`;
+    chatli.innerHTML = chatcontent;
+    chatli.querySelector("p").textContent = message;
+    return chatli;
 }
 
 const generateresponse = (outgoingchat) => {
-    const API_URL ="https://api.openai.com/v1/chat/completions"; 
-    const messageElement=outgoingchat.querySelector("p");
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+    const messageElement = outgoingchat.querySelector("p");
     const requestoptions = {
-        method: "POST" ,
-        headers:{
-            "Content-type" : "application/json" ,
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
             "Authorization": `Bearer ${API_KEY}`
 
         },
-        body:JSON.stringify({
+        body: JSON.stringify({
             "model": "gpt-3.5-turbo",
-            "messages": [{role: "user", content: message }]
+            "messages": [{ role: "user", content: message }]
         })
 
     }
@@ -38,36 +64,44 @@ const generateresponse = (outgoingchat) => {
     }).catch((error) => {
         messageElement.textContent = "Oppss! something went wrong ,please try again";
 
-    }).finally(() => chatbox.scrollTo(0,chatbox.scrollHeight));
+    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
 
 }
+document.addEventListener("keypress", keydown);
 
-chatbutton.addEventListener("click",(e) =>{ 
-    message=chatinput.value.trim();
+chatbutton.addEventListener("click", (e) => {
+  
+    message = chatinput.value.trim();
 
-// console.log(message);
-    if(!message)return;
-    chatinput.value="";
+    // console.log(message);
+    if (!message) return;
+    chatinput.value = "";
 
-chatbox.appendChild(createchatlist(message,"incoming"));
-chatbox.scrollTo(0,chatbox.scrollHeight);
+    chatbox.appendChild(createchatlist(message, "incoming"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
 
-setTimeout(()=>{
-        const outgoingchat=createchatlist("Thinking.....","outgoing")
+    setTimeout(() => {
+        const outgoingchat = createchatlist("Thinking.....", "outgoing")
         chatbox.appendChild(outgoingchat);
-        chatbox.scrollTo(0,chatbox.scrollHeight);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
         generateresponse(outgoingchat);
 
-},100);
+    }, 100);
 
-      
+    
+
+
 });
 
-man.addEventListener("click" ,(e) => {
-    document.body.classList.toggle("active");     
+
+
+
+man.addEventListener("click", (e) => {
+    document.body.classList.toggle("active");
 });
-cross.addEventListener("click" ,(e) => {
-    document.body.classList.add("active");     
+cross.addEventListener("click", (e) => {
+    document.body.classList.add("active");
 });
+
 
 
